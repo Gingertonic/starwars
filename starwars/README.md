8d43b03f8195c61f9707776905d4232d61f71dcc
Beth Schofield <bethmschofield@gmail.com>
2019-08-09 14:58:45 +0000

    fresh start

--- starwars/README.md
+++
@@ -1,5 +0,0 @@
-# Awesome Star Wars App
-
-## Instructions
-
-Run `ruby red_one.rb` and stand by!
--- starwars/red_one.rb
+++
@@ -1,7 +0,0 @@
-puts "Standing by..."
-
-def red_who(name)
-  puts "Red #{name} standing by."
-end 
-
-red_who("Paul")
